8f39264b40eb1034ed0da3bcee502e638fc84713
patrickgdrouin <patrickdrouin@mac.com>
2025-03-17 16:31:26 +0000

    backing up work

---
+++ character_info.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Satrwars lab | Input and print"""
+
+# prompt the user to enter their favorite Satwars character
+fav_char = input("who is your favorite Starwars charatere: ")
+
+# print a message using the input
+print(f"wow! {fav_char} is a great choice!")
+
---
+++ force_level.py
@@ -0,0 +1,7 @@
+# asking user charctere and forces level
+char_name = input("what is a stawrars charactere name: ")
+force_lvl = input(f"what is the level of force of {char_name}:")
+
+# printing charactere name and force lvl
+print(char_name, " is a force user of ", force_lvl, " level")
+
