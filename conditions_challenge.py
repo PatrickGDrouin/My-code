4dce97b346de020eb9f0c68c9777a9deb35597e7
patrickgdrouin <patrickdrouin@mac.com>
2025-03-17 21:41:56 +0000

    backing up work

---
+++ conditions_challenge.py
@@ -0,0 +1,14 @@
+main_dinosaur  = input("what is the main attraction at jurassic park?:").lower()
+
+if main_dinosaur == "t-rex":
+    print("good guess")
+else:
+    print("sadly it is the t-rex")
+
+dna_type = input("what is the DNA use to fill the gaps in the dinosaur genome?:").lower()
+
+if dna_type == "frog":
+    print("good guess")
+else:
+    print("sadly its a frog")
+
