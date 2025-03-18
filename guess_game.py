5b1a7782704fa28f4d78d7fca0812551aa5eec93
patrickgdrouin <patrickdrouin@mac.com>
2025-03-18 15:18:34 +0000

    backing up work

--- guess_game.py
+++ guess_game.py
@@ -2,17 +2,28 @@ import random
 
 # Generate a random number between 1 and 10
 secret_number = random.randint(1, 10)
-num_found = False
+turn = 0
 
-while num_found == False:
-    guess = int(input("Guess the number between 1 and 10: "))
+while turn < 4:
+    turn += 1
+    guess = input("Guess the number between 1 and 10: ")
 
+    if not guess.isdigit():
+        print("please enter a valide  number!")
+        continue
+    guess = int(guess)
     if guess < 1 or guess > 10:
         print("Invalid guess. Please enter a number between 1 and 10.")
+        continue
     elif guess == secret_number:
-        print("You guessed it! Great job!")
-        num_found = True
+        print(f"You guessed it in {turn}! Great job!")
+        break
     elif guess > secret_number:
         print("Too high!")
     elif guess < secret_number:
         print("Too low!")
+   
+    if turn == 3:
+        print(f"{turn} turn is too many, you loose!!")
+        break
+
