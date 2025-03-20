aebe13c32201c3c8d03364e22e3936d528e3c782
patrickgdrouin <patrickdrouin@mac.com>
2025-03-20 20:48:01 +0000

    Errors redirects and limits

--- flaskapi/limitapis.py
+++ flaskapi/limitapis.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 limiter = Limiter(
     app = app,
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
+    default_limits=["10  per day", "50 per hour"]
 )
 
 # we now have TWO decorators on our function
